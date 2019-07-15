7b990c801a0a1a7eea987dce83aa8993fdd8f4c7
Virginia Thayer <virginia@stripe.com>
2019-07-15 13:10:42 -0700

    Added score reset buttons that set a team's score back to 0

--- src/App.js
+++ src/App.js
@@ -34,10 +34,12 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick={ ()=> setHomeScore(homeScore + 7) } className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick={ ()=> setHomeScore(homeScore + 3) } className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={ ()=> setHomeScore(0) }>Reset Home Score</button>
         </div>
         <div className="awayButtons">
           <button onClick={ () => setAwayScore(awayScore + 7) } className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick={ ()=> setAwayScore(awayScore + 3) } className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={ ()=> setAwayScore(0) }>Reset Away Score</button>
         </div>
       </section>
     </div>
